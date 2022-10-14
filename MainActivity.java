57dfafc922793ce626f6c55583e23c3aa3da9015
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-14 14:24:58 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,47 @@
+package status.bar.app;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.app.*;
+import android.os.*;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.view.KeyEvent.Callback;
+import	android.view.KeyEvent;
+import android.view.MenuInflater;
+import 	android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.view.ViewConfiguration;
+
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+
+import android.content.Context;
+
+
+
+public class MainActivity extends Activity
+{
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+	public void  onmenu1_click(View v) {
+		setTitle("click");
+		final TextView helloTextView = (TextView) findViewById(R.id.statusBar);
+		helloTextView.setText("click the button");
+
+	} 
+
+}
+
